590ff4eaab64fdfd0a62f5ee60612b897d65979e
Kaixl <hostkaixl@gmail.com>
2020-05-22 15:41:30 +0200

    zmiany

---
+++ PaperRockScissors/PaperRockScissors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PaperRockScissors
+{
+    class Program
+    {
+        enum Choice
+        {
+            Paper = 1,
+            Rock = 2,
+            Scissors = 3
+        }
+
+
+        static void Main(string[] args)
+        {
+            Random rndChoice = new Random();
+
+            Console.WriteLine("This is \"Rock, paper, scissors \" game.");
+            Console.WriteLine("(1) - Paper");
+            Console.WriteLine("(2) - Rock");
+            Console.WriteLine("(3) - Scissors");
+            Console.WriteLine("Make a choice : ");
+
+            int userInput = int.Parse(Console.ReadLine());
+            var computerInput = (Choice)rndChoice.Next(1, 4);
+
+            Console.WriteLine($"Your choice : {(Choice)userInput}");
+            Console.WriteLine($"Computer choice : {computerInput}");
+        }
+    }
+}
