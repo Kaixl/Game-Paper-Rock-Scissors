38ef57436e4f6bcbc0b57945db2087e287aed1c2
Kaixl <hostkaixl@gmail.com>
2020-05-23 10:57:51 +0200

    Zmiany03

--- PaperRockScissors/PaperRockScissors/Program.cs
+++ PaperRockScissors/PaperRockScissors/Program.cs
@@ -16,6 +16,15 @@ namespace PaperRockScissors
         {
             Random rndChoice = new Random();
 
+            int userScore = 0;
+            int computerScore = 0;
+
+            bool winner = false;
+
+            do
+            {
+
+            
             Console.WriteLine("This is \"Rock, paper, scissors \" game.");
             Console.WriteLine("(1) - Paper");
             Console.WriteLine("(2) - Rock");
@@ -25,10 +34,12 @@ namespace PaperRockScissors
             string userInput = Console.ReadLine();
             var computerInput = (Choice)rndChoice.Next(1, 4);
 
+            
+
             bool check = false;
             
 
-            //  WYJĄKI
+            //  WYJĄTKI
             try
             {
                 int user = int.Parse(userInput);
@@ -53,6 +64,17 @@ namespace PaperRockScissors
                 Console.ForegroundColor = ConsoleColor.White;
                 check = true;
             }
+
+            if(int.Parse(userInput) > 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your argument is out of range.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Choose the one of the option from the menu above.");
+                Console.ForegroundColor = ConsoleColor.White;
+                check = true;
+            }
             
             if(check == true)
             {
@@ -62,9 +84,37 @@ namespace PaperRockScissors
             {
                 Console.WriteLine($"Your choice : {(Choice)int.Parse(userInput)}");
                 Console.WriteLine($"Computer choice : {computerInput}");
-            }            
+            }          
+            
+            if(int.Parse(userInput) == 1 && computerInput == Choice.Rock || int.Parse(userInput) == 2 && computerInput == Choice.Scissors || int.Parse(userInput) == 3 && computerInput == Choice.Paper)
+            {
+                userScore++;
+                Console.WriteLine("{0} beats {1}. User get's point." ,(Choice)int.Parse(userInput), computerInput);
+                Console.WriteLine($"User score: {userScore}");
+                Console.WriteLine($"Computer score: {computerScore}");
+            }
+            else if(int.Parse(userInput) == 1 && computerInput == Choice.Scissors || int.Parse(userInput) == 2 && computerInput == Choice.Paper || int.Parse(userInput) == 3 && computerInput == Choice.Rock)
+            {
+                computerScore++;
+                Console.WriteLine("{0} beats {1}. Computer get's point.", computerInput, (Choice)int.Parse(userInput));
+                Console.WriteLine($"User score: {userScore}");
+                Console.WriteLine($"Computer score: {computerScore}");
+            }
+            else if(int.Parse(userInput) == 1 && computerInput == Choice.Paper || int.Parse(userInput) == 2 && computerInput == Choice.Rock || int.Parse(userInput) == 3 && computerInput == Choice.Scissors)
+            {
+                Console.WriteLine("It's a draw. Go on, try again.");
+            }
 
+            if(userScore == 3)
+                {
+                    winner = true;
+                }
+            else if(computerScore == 3)
+                {
+                    winner = true;
+                }
 
+            } while (!winner);
         }
     }
 }
